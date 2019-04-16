0da3675200486da5042d4bfb6cbd360f55c68613
lukaslukas <eiglerhardingl@gmail.com>
2019-04-16 10:10:40 -0400

    updated animation logic

--- assets/script.js
+++ assets/script.js
@@ -2,20 +2,49 @@ var Site = {};
 
 Site.ambientBackground = function(){
 	var s = Snap("#background");
-	Snap.load("/assets/arcs/ligeti-arc.svg", function(arc){
-		//svg loaded
-		var viewBox = s.attr("viewBox"),
-				paths = arc.selectAll("path");
+	
+	var svgArcs = ["/assets/arcs/ligeti-arc-all.svg"];
 
-		paths.forEach(function(thisPath, index){
-			s.append(thisPath)
+	var animationCycle = function(svgArc){
+		Snap.load(svgArc, function(arc){
+			//svg loaded
+			var viewBox = s.attr("viewBox"),
+					paths = arc.selectAll("path");
 
-			thisPath.animate({
-				fill: "#fff"
-			}, 1000)
+			var loadAndSlide = function(paths, speed, index){
 
-		}) // end of forEach
-	})
+				var thisPath = paths[index];
+				var colors = ["#692E5C", "#8BDEFF", "#FFFA2E", "#FF5CE5", "#692EA5", "#A2004D", "#692EFF", "#00E733"]
+
+				thisPath.transform('translate(0 900)').attr({ fill: colors[index] });
+
+				s.append(thisPath)
+				// console.log(Snap.parsePathString(thisPath))
+				thisPath.animate({
+					transform: 'translate(0 -900)'
+				}, speed, mina.linear, function(){
+					console.log(thisPath.attr("class"))
+					// remove path?
+					thisPath.remove()
+					var randomPath = Math.round(Math.random()*(paths.length-1));
+					loadAndSlide(paths, speed, randomPath)
+				})
+
+			}
+			
+			// bugs include if it's the same class on both animations, it deletes itself
+			// also need to figure out how to allow for consistent overlap (negative transform)
+
+			loadAndSlide(paths, 4000, 0)
+			// setTimeout(function(){
+			// 	loadAndSlide(paths, 4000, 0)				
+			// }, 2000)
+
+		})
+	}
+
+	animationCycle(svgArcs[0])
+	
 }
 
 
